64f88343d2e923d55fc22595a04f3cd069c5cb8c
Peter A. Jonsson <peter.jonsson@ri.se>
2026-08-27 15:34:37 +0200

    org-config/des: add des-env-setup.sh
    Put the non-secret parts of the
    configuration into the repository
    instead of having extra config values
    stored in the CI settings.

---
+++ org-config/des/des-env-setup.sh
@@ -0,0 +1,6 @@
+# The non-secret parts of the environment configuration used by the CI.
+# Sourced by developers that want to reproduce a build.
+export CI_REGISTRY=harbor.digitalearth.se
+export CI_REGISTRY_BASE_PATH=des-images
+export CI_REGISTRY_UPSTREAM_BASE_PATH=des-upstream
+export ORGANIZATION_NAME=des
