0a74e6b971b6ebc495347ccee5d665a3c5944619
Peter A. Jonsson <peter.jonsson@ri.se>
2026-09-13 10:59:06 +0200

    CI: load variables from org-config
    Put the non-secret variables into
    the org-config repository and load
    them from there instead of having
    them hidden in the CI configuration.
    This makes it possible to source
    the file and have a similar
    configuration as the CI does when
    building locally.

--- org-config/des/des-env-setup.sh
+++ org-config/des/des-env-setup.sh
@@ -1,6 +1,8 @@
 # The non-secret parts of the environment configuration used by the CI.
 # Sourced by developers that want to reproduce a build.
+# Comments must start at column 1, everything else goes into the environment.
 export CI_REGISTRY=harbor.digitalearth.se
 export CI_REGISTRY_BASE_PATH=des-images
 export CI_REGISTRY_UPSTREAM_BASE_PATH=des-upstream
 export ORGANIZATION_NAME=des
+export UPSTREAM_IMAGES="des-cloudnative-pg:1.30.0 des-cloudnativepg-pgbouncer:1 des-cloudnativepg-postgres:18 des-cloudnativepg-postgis:3 des-postgres:18-bookworm des-postgis:18-3.6 des-valkey:9.1"
